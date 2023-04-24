55355c7bfbe53c18c1941a1f82ea3cba9a3a9319
Walker-mfv <laiphuc2310@gmail.com>
2023-04-24 21:58:22 +0700

    add space heading description course

--- app/modules/client/pages/CourseDetail/Sections/CourseDescription.tsx
+++ app/modules/client/pages/CourseDetail/Sections/CourseDescription.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
-import { Text, Collapse, Box } from '@chakra-ui/react'
+import { Text, Collapse, Box, Stack } from '@chakra-ui/react'
 import React from 'react'
 import ContentCard from '../../../components/ContentCard'
 import { useCourseDetailDescription } from '../../../queries/course-detail-query.hook'
@@ -12,30 +12,32 @@ function CourseDescription() {
 
   return (
     <ContentCard title="Description" border="none" spacing={5}>
-      <Box style={{ WebkitMaskImage: isShow ? 'unset' : 'linear-gradient(#ffffff,#ffffff,rgba(255,255,255,0)' }}>
-        <Collapse startingHeight={'14rem'} in={isShow}>
-          <div className="des-html" dangerouslySetInnerHTML={{ __html: description || '' }}></div>
-        </Collapse>
-      </Box>
-      <Text
-        onClick={handleToggle}
-        color={'purple.600'}
-        fontSize={'sm'}
-        fontWeight={'bold'}
-        cursor={'pointer'}
-        _hover={{ color: 'purple.700' }}
-      >
-        Show
-        {isShow ? (
-          <>
-            Less <ChevronUpIcon fontSize={'md'} />
-          </>
-        ) : (
-          <>
-            More <ChevronDownIcon fontSize={'md'} />
-          </>
-        )}
-      </Text>
+      <Stack spacing={2}>
+        <Box style={{ WebkitMaskImage: isShow ? 'unset' : 'linear-gradient(#ffffff,#ffffff,rgba(255,255,255,0)' }}>
+          <Collapse startingHeight={'14rem'} in={isShow}>
+            <div className="des-html" dangerouslySetInnerHTML={{ __html: description || '' }}></div>
+          </Collapse>
+        </Box>
+        <Text
+          onClick={handleToggle}
+          color={'purple.600'}
+          fontSize={'sm'}
+          fontWeight={'bold'}
+          cursor={'pointer'}
+          _hover={{ color: 'purple.700' }}
+        >
+          Show
+          {isShow ? (
+            <>
+              &nbsp;less <ChevronUpIcon fontSize={'md'} />
+            </>
+          ) : (
+            <>
+              &nbsp;more <ChevronDownIcon fontSize={'md'} />
+            </>
+          )}
+        </Text>
+      </Stack>
     </ContentCard>
   )
 }
