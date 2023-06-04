474488c0dfcabaae2af25fd5f5f1e28483cee667
Walker-mfv <laiphuc2310@gmail.com>
2023-06-04 20:23:15 +0700

    add attribute form profile

--- app/modules/client/pages/ProfilePage/ProfileForm.tsx
+++ app/modules/client/pages/ProfilePage/ProfileForm.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from '@auth/providers/auth.provider'
 import { RQK_AUTH_USER } from '@auth/queries/auth-user-query.hook'
-import { Button, GridItem, SimpleGrid, Stack } from '@chakra-ui/react'
+import { Button, GridItem, SimpleGrid, Stack, Text } from '@chakra-ui/react'
 import { yupResolver } from '@hookform/resolvers/yup'
 import EditableAvatar from '@shared/components/EditableAvatar'
 import MyInput from '@shared/components/form-set/MyInput'
@@ -8,13 +8,15 @@ import MyProgressBar from '@shared/components/MyProgress'
 import { useAppToast } from '@shared/hooks/app-toast.hook'
 import { useUploadImage } from '@shared/hooks/upload-image.hook'
 import { IProfile, IUser } from '@shared/interfaces/models/user.interface'
+import Editor from '@shared/parts/Editor/Editor'
 import { apiEditProfile } from 'app/apis/user/user.api'
 import FieldLabel from 'app/utils/constants/field-label.constant'
 import { AVATAR_DIR } from 'app/utils/constants/firebase.constant'
 import FormMsg from 'app/utils/constants/form-message.constant'
+import Helper from 'app/utils/helpers/helper.helper'
 import NotifyHelper from 'app/utils/helpers/notify.helper'
-import React, { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useEffect, useMemo, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { useQueryClient } from 'react-query'
 import * as yup from 'yup'
 
@@ -24,6 +26,8 @@ type FormData = {
   avatar: string
   phone?: string
   address?: string
+  headline?: string
+  biography?: string
 }
 
 const ProfileForm = () => {
@@ -36,14 +40,28 @@ const ProfileForm = () => {
   const toast = useAppToast()
   const [item, setItem] = useState<IProfile>()
   const [isDisabled, setDisabled] = useState<boolean>(false)
+  const isStudent = useMemo(() => user?.role.name === 'Student', [user?.role.name])
 
   const { handleUpload, uploadProgress, getImgSrcFuncRef } = useUploadImage(AVATAR_DIR, item?.avatar)
   const initialValues = yup.object({
     firstName: yup.string().required(FormMsg.required),
     lastName: yup.string().required(FormMsg.required),
     avatar: yup.string().nullable(),
-    phone: yup.string().nullable(),
+    phone: yup.string().test('is empty', FormMsg.required, (value: any) => {
+      return isStudent || !!value
+    }),
     address: yup.string().nullable(),
+    headline: yup.string().test('is empty', FormMsg.required, (value: any) => {
+      return isStudent || !!value
+    }),
+    biography: yup
+      .string()
+      .test('is empty', FormMsg.required, (value: any) => {
+        return isStudent || !!Helper.getTextFromHTML(value)
+      })
+      .test('min length', 'must be at least 50 characters', (value: any) => {
+        return isStudent || Helper.getTextFromHTML(value).length > 50
+      }),
   })
 
   // FORM HOOKS
@@ -52,6 +70,7 @@ const ProfileForm = () => {
     handleSubmit,
     formState: { errors, isDirty },
     reset,
+    control,
   } = useForm<FormData>({
     resolver: yupResolver(initialValues),
   })
@@ -63,6 +82,8 @@ const ProfileForm = () => {
       lastName: user?.profile.lastName || '',
       avatar: user?.profile.avatar || '',
       phone: user?.profile.phone,
+      headline: user?.profile.headline || '',
+      biography: user?.profile.biography || '',
       address: user?.profile.address,
     }
     setItem(user?.profile)
@@ -81,6 +102,8 @@ const ProfileForm = () => {
           avatar: imgSrc,
           phone: values.phone,
           address: values.address,
+          headline: values.headline,
+          biography: values.biography,
         } as IProfile,
       }
       try {
@@ -151,10 +174,64 @@ const ProfileForm = () => {
                   register={register}
                   error={errors.phone}
                   placeholder="Phone number"
+                  required={!isStudent}
                 />
               </GridItem>
             </SimpleGrid>
-
+            {!isStudent && (
+              <>
+                <SimpleGrid columns={1} spacing={2}>
+                  <GridItem
+                    colSpan={{
+                      base: 2,
+                      md: 1,
+                    }}
+                  ></GridItem>
+                  <GridItem
+                    colSpan={{
+                      base: 2,
+                      md: 1,
+                    }}
+                  >
+                    <MyInput
+                      field="headline"
+                      label={FieldLabel['user.headline']}
+                      register={register}
+                      error={errors.headline}
+                      placeholder="Headline"
+                      helperText={'Add a professional headline like, "Instructor at Udemy" or "Architect."'}
+                      required
+                    />
+                  </GridItem>
+                </SimpleGrid>
+                <Stack spacing={2}>
+                  <Controller
+                    control={control}
+                    name="biography"
+                    render={({ field }) => (
+                      <Editor
+                        field="biography"
+                        label={FieldLabel['user.biography']}
+                        placeholder="Biography"
+                        className="editor-input"
+                        value={field.value}
+                        onChange={field.onChange}
+                        required
+                      />
+                    )}
+                  />
+                  {errors.biography && (
+                    <Text color={'red.500'} fontSize={'sm'}>
+                      Biography {errors.biography?.message}
+                    </Text>
+                  )}
+                  <Text color="gray.500" fontSize="sm">
+                    Your instructor biography should emphasize your experience and expertise. It should have at least 50
+                    characters.
+                  </Text>
+                </Stack>
+              </>
+            )}
             <MyInput
               field="address"
               label={FieldLabel['user.address']}
